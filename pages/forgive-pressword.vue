53d368e3cb3b75bab9ecbb6a322841067119c545
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2026-02-18 15:01:06 +0700

    forgive-pressword done

---
+++ pages/forgive-pressword.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="min-h-screen flex flex-col md:flex-row bg-gray-100">
+
+  
+  <div class="md:w-1/2 flex flex-col justify-center px-8 lg:px-16 py-12 bg-[#E5E7EB]">
+    
+    <h1 class="text-3xl sm:text-4xl lg:text-5xl font-bold text-[#0F3D57] mb-10">
+      Take the first step toward a smarter talent strategy
+    </h1>
+
+    <div class="flex justify-center md:justify-start">
+      <img 
+        src="../assets/css/img/Artboard 1.webp"
+        alt="Recruitment Illustration"
+        class="w-64 sm:w-72 lg:w-80 object-contain"
+      >
+    </div>
+
+  </div>
+
+
+  
+  <div class="md:w-1/2 flex items-center justify-center px-6 py-12">
+
+    <div class="bg-white w-full max-w-md p-8 rounded-2xl shadow-lg">
+
+      <h2 class="text-xl sm:text-2xl font-semibold text-center mb-6">
+        Sign In to WORKINGNA
+      </h2>
+
+      
+      
+
+      
+      <div class="mb-4">
+        <label class="block mb-1 text-sm font-medium">Email*</label>
+        <input 
+          type="email"
+          placeholder="name@company.com"
+          class="w-full border rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-400"
+        >
+      </div>
+      <div class="flex justify-between mb-6">
+        <a href=""></a><button class="w-1/2 py-2 border rounded-full mr-2 hover:bg-gray-100 transition">
+          Cencel
+        </button>
+        <button class="w-1/2 py-2 border border-red-400 text-red-500 rounded-full ml-2 hover:bg-red-50 transition">
+          Send Verify Code
+        </button>
+      </div>
+    </div>
+    
+</div>
+</section>
+</template>
